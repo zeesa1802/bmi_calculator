6aa3de6a5576adf15dd77fa505f077732217ee93
Zeeshan <zeesa18.2@gmail.com>
2022-06-11 20:02:26 +0500

    Applied BMI formula to complete the task

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("here")
+let markHigherBmi;
+
+// let markWeight = 78;
+// let markHeight = 1.69;
+
+// let johnWeight = 92;
+// let johnHeight = 1.95;
+
+let markWeight = 95;
+let markHeight = 1.88;
+
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+const markBmi = markWeight / (markHeight ** 2);
+const johnBmi = johnWeight / (johnHeight ** 2);
+
+markHigherBmi = markBmi > johnBmi;
+
+console.log(markBmi, johnBmi)
+console.log(markHigherBmi);
